715c1b1d2ed8e7628a449abf4afd1bea88c1506b
Fu Xiang <20948660@qq.com>
2015-05-01 11:42:09 +0800

    增加一个返回固定格式字符串函数

--- TinyMetroWpfLibrary/TinyMetroWpfLibrary.Utility/StringHexConverter.cs
+++ TinyMetroWpfLibrary/TinyMetroWpfLibrary.Utility/StringHexConverter.cs
@@ -5,6 +5,27 @@ namespace TinyMetroWpfLibrary.Utility
 {
     public class StringHexConverter
     {
+        /// <summary>
+        /// 返回数据的固定长度字符串，如2.34，固定长度为5，小数点位置在3，则返回00234，小数点位置在2，则返回02340
+        /// </summary>
+        /// <param name="src"></param>
+        /// <param name="dotindex"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static string GetFormedString(string src, int dotindex, int length)
+        {
+            int SrcDotIndex = src.IndexOf('.');
+            src = src.Replace(".", "");
+            if (SrcDotIndex > dotindex)
+            {
+                throw (new Exception("GetFormedString输入格式错误！"));
+            }
+            int move = dotindex - SrcDotIndex;
+            src = src.PadLeft(src.Length + move, '0');
+            src = src.PadRight(length, '0');
+            return src;
+
+        }
         public static string ConvertStrToHex(string str)
         {
             string data = "";
